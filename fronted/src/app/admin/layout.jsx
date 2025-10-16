523ad5286992e3b82db41ab9fc61a0872a996236
Robert Raul <lordnebr4@gmail.com>
2025-10-16 14:41:16 -0500

    Revert "Merge branch 'main' of https://github.com/RobertRaul/ecommerce-project"
    This reverts commit 5a59c74ac544ca41971ac80bf0975b52244a2fd8, reversing
    changes made to a234b3ff4cb6f8ad2ddaac0b69c5f685ef2b79b2.

---
+++ fronted/src/app/admin/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: 'Admin Panel - ECOMMERCE',
+    description: 'Panel de administración',
+};
+
+export default function Layout({ children }) {
+    return children;
+}
